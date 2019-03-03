8fad58aad232b554377415974e588b6d6df832c8
abdul.halim <abdul.halim@axiatadigital.com>
2019-03-03 13:31:46 +0800

    Create Plot 1.R

---
+++ Plot 1.R
@@ -0,0 +1,9 @@
+library(dplyr)
+srcfile <- read.table("D:\\A1881\\Documents\\GitHub\\coursera\\Data Science Basic\\Week 1 - Assignment\\household_power_consumption.txt", sep=";", header=TRUE)
+df_srcfile <- tbl_df(srcfile)
+dataToUse <- subset(df_srcfile,df_srcfile$Date=="1/2/2007" | df_srcfile$Date=="2/2/2007")
+col_global_active_power <- dataToUse$Global_active_power
+use_global_active_power <- as.numeric(as.character(col_global_active_power))
+png("plotadfg.png",width=480,height=480)
+hist(use_global_active_power,xlab = "Global Active Power (kilowatts)", col="red", main="Global Active Power")
+dev.off()
