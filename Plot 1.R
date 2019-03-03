3fc0d78dcfba7bee9552e7c288619fdccc0622ef
abdul.halim <abdul.halim@axiatadigital.com>
2019-03-03 13:32:38 +0800

    Update Plot 1.R

--- Plot 1.R
+++ Plot 1.R
@@ -4,6 +4,6 @@ df_srcfile <- tbl_df(srcfile)
 dataToUse <- subset(df_srcfile,df_srcfile$Date=="1/2/2007" | df_srcfile$Date=="2/2/2007")
 col_global_active_power <- dataToUse$Global_active_power
 use_global_active_power <- as.numeric(as.character(col_global_active_power))
-png("plotadfg.png",width=480,height=480)
+png("plot1.png",width=480,height=480)
 hist(use_global_active_power,xlab = "Global Active Power (kilowatts)", col="red", main="Global Active Power")
 dev.off()
